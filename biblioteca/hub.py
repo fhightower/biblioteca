1070f3784f05d5c0208f580500e106892e6b4d63
Floyd Hightower <floyd.hightower27@gmail.com>
2020-08-29 21:04:20 -0400

    Updating hub for democritus core changes

--- biblioteca/hub.py
+++ biblioteca/hub.py
@@ -3,22 +3,22 @@
 
 import uuid
 
-from democritus import pdfRead, isUrl, lowercase, urlFileName, urlDomain, reqGet, esAdd, uuid3, html2Text
+from democritus_core import json_write, pdf_read, is_url, lowercase, url_file_name, url_domain, get, uuid3, html_to_text, map_first_arg
 
 NAMESPACE = uuid.UUID(bytes=b'biblioteca000000')
 
 
 def enrich_data(new_data):
-    """."""
+    """Enrich the given data appropriately for its type."""
     es_data = {}
 
-    if isUrl(new_data):
+    if is_url(new_data):
         es_data['url'] = new_data
-        url_file_ending = lowercase(urlFileName(new_data))
-        domain_name = lowercase(urlDomain(new_data))
+        url_file_ending = lowercase(url_file_name(new_data))
+        domain_name = lowercase(url_domain(new_data))
 
         if url_file_ending.endswith('.pdf'):
-            results = pdfRead(new_data)
+            results = pdf_read(new_data)
             if any(results):
                 es_data['raw'] = '\n\n'.join(results)
         else:
@@ -31,32 +31,19 @@ def enrich_data(new_data):
             elif 'wikipedia' in domain_name:
                 pass
             else:
-                raw_data = reqGet(new_data)
+                raw_data = get(new_data)
                 if raw_data:
-                    es_data['raw'] = html2Text(raw_data)
+                    es_data['raw'] = html_to_text(raw_data)
     else:
         print('Something that was not a url was given and we aren\'t ready for this kind of data yet: {}'.format(new_data))
 
     return es_data
 
 
-def add(new_data, entry_type='library', id=None, note=None):
+@map_first_arg
+def add(new_data):
     """Add the new data to the library."""
-    # handle incoming data in a list
-    if isinstance(new_data, list):
-        results = []
-        for data in new_data:
-            results.append(add(data, entry_type=entry_type, id=id, note=note))
-        return results
-
     enriched_data = enrich_data(new_data)
 
-    if note:
-        enriched_data['note'] = note
-
-    # TODO: may want to add a check to see if the item at the given id exists
-    # TODO: improve error handling
-    if id:
-        return esAdd('biblioteca', entry_type, enriched_data, id=id)
-    else:
-        return esAdd('biblioteca', entry_type, enriched_data, id=uuid3(enriched_data['url'], namespace=NAMESPACE))
+    # TODO: need to create a file path somehow
+    json_write(enriched_data, )
