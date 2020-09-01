b810a9eb10d2299667a5c2fd3625f280297bf12e
Floyd Hightower <floyd.hightower27@gmail.com>
2020-09-01 16:30:04 -0400

    Adding basic ability to write content to a file for the given data

--- biblioteca/hub.py
+++ biblioteca/hub.py
@@ -1,49 +1,59 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+import datetime
 import uuid
 
-from democritus_core import json_write, pdf_read, is_url, lowercase, url_file_name, url_domain, get, uuid3, html_to_text, map_first_arg
+from democritus_core import json_write, pdf_read, is_url, lowercase, url_file_name, url_domain, get, uuid3, html_to_text, map_first_arg, home_directory_join, file_name_escape
 
 NAMESPACE = uuid.UUID(bytes=b'biblioteca000000')
+BASE_PATH = home_directory_join('biblioteca/')
 
 
 def enrich_data(new_data):
     """Enrich the given data appropriately for its type."""
-    es_data = {}
+    raw_data = None
+    enrich_data = None
+    meta_data = {
+        'first_collected': str(datetime.date.today())
+    }
 
     if is_url(new_data):
-        es_data['url'] = new_data
+        meta_data['url'] = new_data
         url_file_ending = lowercase(url_file_name(new_data))
         domain_name = lowercase(url_domain(new_data))
 
         if url_file_ending.endswith('.pdf'):
             results = pdf_read(new_data)
             if any(results):
-                es_data['raw'] = '\n\n'.join(results)
+                enrich_data = '\n\n'.join(results)
         else:
-            if domain_name in ['youtube.com', 'youtu.be']:
-                pass
-            elif domain_name == 'github.com':
-                pass
-            elif domain_name == 'twitter.com':
-                pass
-            elif 'wikipedia' in domain_name:
-                pass
-            else:
-                raw_data = get(new_data)
-                if raw_data:
-                    es_data['raw'] = html_to_text(raw_data)
+            # if domain_name in ['youtube.com', 'youtu.be']:
+            #     pass
+            # elif domain_name == 'github.com':
+            #     pass
+            # elif domain_name == 'twitter.com':
+            #     pass
+            # elif 'wikipedia' in domain_name:
+            #     pass
+            # else:
+            #     pass
+            raw_data = get(new_data)
+            enriched_data = html_to_text(raw_data)
     else:
-        print('Something that was not a url was given and we aren\'t ready for this kind of data yet: {}'.format(new_data))
+        raw_data = new_data
 
-    return es_data
+    return raw_data, enriched_data, meta_data
 
 
 @map_first_arg
-def add(new_data):
+def add(new_data, name=None):
     """Add the new data to the library."""
-    enriched_data = enrich_data(new_data)
+    raw_data, enriched_data, meta_data = enrich_data(new_data)
 
-    # TODO: need to create a file path somehow
-    json_write(enriched_data, )
+    if name:
+        meta_data['original_name'] = name
+        name = file_name_escape(name)
+    else:
+        name = uuid3(namespace=NAMESPACE)
+    file_write(os.path.join(BASE_PATH, f'{name}.raw'), new_data)
+    if enriched_data:
+        file_write(os.path.join(BASE_PATH, f'{name}.enriched'), enriched_data)
+    json_write(os.path.join(BASE_PATH, f'{name}.meta'), meta_data)
